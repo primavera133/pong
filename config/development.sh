8b6744e27f839fa747402201da14bdf5a8ce2ee7
Jonas Myrenås <jonas@myrenas.se>
2017-04-10 21:42:35 +0200

    heroku deploy

--- config/development.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export MONGODB_URI=mongodb://app:Jyt8xXY5neKnCNdT@ds145780.mlab.com:45780/heroku_96h6p89x
-export AUTH_EMAIL=jonas@myrenas.se
-export AUTH_PWD=123qqwwee123
-export AUTH_TTL=86400000
-export AUTH_KEY=EgPrcWSqwCkaV5N6BkpAAYdac3MgpFhBvNZ5hLtJkdvLwu2C5PsamQADMt9pbE5R
-export IO_SOCKET=http://localhost:8000
-export PORT=8000
